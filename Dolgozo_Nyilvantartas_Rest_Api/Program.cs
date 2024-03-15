3de6bd2c8ba96dd686d39e8bb795dcb737a90159
zeteny12 <zetus0512@gmail.com>
2024-03-15 22:05:44 +0100

    Add project files.

---
+++ Dolgozo_Nyilvantartas_Rest_Api/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Cache;
+
+namespace Dolgozo_Nyilvantartas_Rest_Api
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            DolgozokNyilvantartasa();
+
+            Console.ReadKey();
+        }
+
+        private static async Task DolgozokNyilvantartasa()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("https://retoolapi.dev/Kc6xuH/data");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonString = await response.Content.ReadAsStringAsync();
+                        Dolgozok[] dolgozok = Dolgozok.FromJson(jsonString);
+
+                        Console.WriteLine($"Elemek száma: {dolgozok.Length}");
+                        Console.WriteLine($"Legmagasabb fizetéssel rendelkező dolgozó: {dolgozok.OrderByDescending(d => d.Salary).First().Name}");
+                        Console.WriteLine($"Fizetése: {dolgozok.OrderByDescending(d => d.Salary).First().Salary}$");
+                        Console.WriteLine($"\nMunkakörök: ");
+
+                        var munkakorok = dolgozok.GroupBy(d => d.Position).Select(g => new { Position = g.Key, Count = g.Count() });
+                        foreach (var munkakor in munkakorok)
+                        {
+                            Console.WriteLine($"\t{munkakor.Position} - {munkakor.Count}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hiba történt a csatlakozás során");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }   
+        }
+    }
+}
